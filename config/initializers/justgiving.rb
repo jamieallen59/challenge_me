6808183647c05d8b7094ed8e3a5ec7c1f1aa2d01
Toan Nguyen <toan@graduate.uwa.edu.au>
2014-08-30 15:14:29 +0100

    added just giving api initialiser

--- config/initializers/justgiving.rb
+++ config/initializers/justgiving.rb
@@ -1,14 +1,7 @@
 require 'just_giving'
 
-JustGiving::Configuration.application_id = "d3a7b8b5"
-
+JustGiving::Configuration.application_id = Rails.application.secrets.just_giving_app_id
 JustGiving::Configuration.ca_path = "/System/Library/OpenSSL/certs" # (defaults to "/usr/lib/ssl/certs")
-
 JustGiving::Configuration.environment = :production
-
-JustGiving::Configuration.username = 'toan' # Needed for actions that require auth
-
-JustGiving::Configuration.password = 'wcXubDLD9GY8QfDkvaobfwL' # Needed for actions that require auth
-
-response = JustGiving::Fundraising.new('ngaiosimpson')
-puts response.inspect
+JustGiving::Configuration.username = Rails.application.secrets.just_giving_username
+JustGiving::Configuration.password = Rails.application.secrets.just_giving_password
