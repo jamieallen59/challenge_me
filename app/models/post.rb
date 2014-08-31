72536cb3f54c55a90bcf65b81e3445e7f2538e5a
Nikesh Ashar <nikeshashar@gmail.com>
2014-08-31 17:06:31 +0100

    Features added: event now has many posts, posts belongs to events. Updated tests and displays

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
 	has_attached_file :picture, :styles => { :medium => "300x300>" }
 	
 	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/ 
+
+	belongs_to :event
 end
--- spec/features/creating_posts_feature_spec.rb
+++ spec/features/creating_posts_feature_spec.rb
@@ -1,22 +1,26 @@
 require 'rails_helper'
 
 describe 'creating posts' do
+	before do 
+		@event = create(:event)
+	end
+
 	it 'adds the post to a form' do
-		visit '/posts'
+		visit event_path(@event)
 		click_link 'New Post'
 		fill_in 'Caption', with: '5k in 30 mins - yay!'
 		click_button 'Create Post'
 
 		expect(page).to have_content '5k in 30 mins - yay!'
-		expect(current_path).to eq '/posts'
+		expect(current_path).to eq event_path(@event)
 	end	
 
 	it 'can attach an image to a post' do 
-		visit '/posts'
+		visit event_path(@event)
 		click_link 'New Post'
 		attach_file 'Picture', Rails.root.join('app/assets/images/run.jpg')
 		click_button 'Create Post'
 		expect(page).to have_css 'img.uploaded-pic'
-		expect(current_path).to eq '/posts'
+		expect(current_path).to eq event_path(@event)
 	end
 end
