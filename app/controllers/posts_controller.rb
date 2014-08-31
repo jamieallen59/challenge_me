89c08c40558fcf997b4f24c138e5763e9b64f8f6
Nikesh Ashar <nikeshashar@gmail.com>
2014-08-31 14:45:00 +0100

    Creating a post via a form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,14 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(params[:post].permit(:caption))
+		@post.save
+		redirect_to '/posts'
+	end
 end
