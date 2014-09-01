fa585b0f94a6f2ad30acf972b738e1432ca3c36e
csharpd <chloedonegan@gmail.com>
2014-09-01 10:39:44 +0100

    Validation added to post caption

--- spec/features/creating_posts_feature_spec.rb
+++ spec/features/creating_posts_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'creating posts' do
-	before do 
+	before do
 		@event = create(:event)
 	end
 
@@ -12,14 +12,28 @@ describe 'creating posts' do
 		click_button 'Create Post'
 		expect(page).to have_content '5k in 30 mins - yay!'
 		expect(current_path).to eq event_path(@event)
-	end	
+	end
 
-	it 'can attach an image to a post' do 
+	it 'can attach an image to a post' do
 		visit event_path(@event)
 		click_link 'New Post'
+		fill_in 'Caption', with: '5k in 30 mins - yay!'
 		attach_file 'Picture', Rails.root.join('app/assets/images/run.jpg')
 		click_button 'Create Post'
 		expect(page).to have_css 'img.uploaded-pic'
 		expect(current_path).to eq event_path(@event)
+	end
+end
+
+describe 'post validation' do
+	before do
+		@event = create(:event)
+	end
+	it "when info isn't valid the post isn't created " do
+		visit event_path(@event)
+		click_link 'New Post'
+		fill_in 'Caption', with: 'yo'
+		click_button 'Create Post'
+		expect(page).to have_content 'Your caption must be greater than two characters'
 	end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
+  context 'Validations' do
+    it ' is valid when the caption is greater than two characters' do
+      post = Post.create(caption: "I'm so speedy")
+      expect(post).to be_valid
+    end
 
+    it 'is not valid when the caption is less than two characters' do
+      post = Post.create(caption: 'Hi')
+      expect(post).to have(1).error_on(:caption)
+    end
+  end
 end
