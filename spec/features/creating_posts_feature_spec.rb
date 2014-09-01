1d5a8e135ebbcb0d26279b1e2ab97e213a8db26e
Nikesh Ashar <nikeshashar@gmail.com>
2014-09-01 14:35:20 +0100

    Toan & Nikesh || Finished creating posts features tests and removed missing image placeholders

--- spec/features/creating_posts_feature_spec.rb
+++ spec/features/creating_posts_feature_spec.rb
@@ -18,7 +18,7 @@ describe 'creating posts' do
 		before do
 			login_as @mary
 		end
-		it 'adds the post to a form' do
+		it 'adds a text post to a form' do
 			visit event_path(@event)
 			click_link 'New Post'
 			fill_in 'Caption', with: '5k in 30 mins - yay!'
@@ -26,31 +26,33 @@ describe 'creating posts' do
 			expect(page).to have_content '5k in 30 mins - yay!'
 			expect(current_path).to eq event_path(@event)
 			expect(page).to have_content 'posted by Mary Perfect'
+			expect(page).not_to have_css 'img.uploaded-pic'
 		end
 
-		# it 'can attach an image to a post' do
-		# 	visit event_path(@event)
-		# 	click_link 'New Post'
-		# 	fill_in 'Caption', with: '5k in 30 mins - yay!'
-		# 	attach_file 'Picture', Rails.root.join('app/assets/images/run.jpg')
-		# 	click_button 'Create Post'
-		# 	expect(page).to have_css 'img.uploaded-pic'
-		# 	expect(current_path).to eq event_path(@event)
-		# end
+		it 'can attach an image to a post' do
+			visit event_path(@event)
+			click_link 'New Post'
+			fill_in 'Caption', with: '5k in 30 mins - yay!'
+			attach_file 'Picture', Rails.root.join('app/assets/images/run.jpg')
+			click_button 'Create Post'
+			expect(page).to have_css 'img.uploaded-pic'
+			expect(current_path).to eq event_path(@event)
+		end
 	end
 
-# end
+end
 
-# describe 'post validation' do
-# 	before do
-# 		mary = create(:user)
-# 		@event = create(:event, user: mary)
-# 	end
-# 	it "when info isn't valid the post isn't created " do
-# 		visit event_path(@event)
-# 		click_link 'New Post'
-# 		fill_in 'Caption', with: 'yo'
-# 		click_button 'Create Post'
-# 		expect(page).to have_content "Your caption must be between 3 and 140 characters"
-# 	end
+describe 'post validation' do
+	before do
+		mary = create(:user)
+		@event = create(:event, user: mary)
+		login_as mary
+	end
+	it "when info isn't valid the post isn't created " do
+		visit event_path(@event)
+		click_link 'New Post'
+		fill_in 'Caption', with: 'yo'
+		click_button 'Create Post'
+		expect(page).to have_content "Your caption must be between 3 and 140 characters"
+	end
 end
