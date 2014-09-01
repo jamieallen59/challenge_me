70acf45c4baa38bd88417475266a2b0f12f95ab2
Nikesh Ashar <nikeshashar@gmail.com>
2014-09-01 17:10:16 +0100

    Fixed Joe's merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,10 @@ Rails.application.routes.draw do
       resource :comments
     end
   end
-<<<<<<< HEAD
 
   # resources :posts do
   #   resource :comments
   # end
  root 'welcome#index'
-=======
- root 'events#index'
->>>>>>> 91716b45a4bb8d13e3008fa7ddb7f776880b5d53
+
 end
