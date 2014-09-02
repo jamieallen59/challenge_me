8eba825cf35aa27bef70985f1e1c36f7113e465a
Nikesh Ashar <nikeshashar@gmail.com>
2014-09-02 14:53:26 +0100

    Nikesh & Toan | Added link to edit posts

---
+++ spec/features/editing_posts_feature_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'Editing Posts' do 
+	before do
+    @mary = create(:user)
+    @fred = create(:fred)
+    @event = create(:event, user: @mary)
+    @post = create(:post, event: @event, user: @mary)
+  end
+
+  context 'logged in as the post owner' do 
+  	before do 
+  		login_as @mary
+      visit event_path(@event)
+  	end
+
+  	it 'should have a link to edit the post' do
+      expect(page).to have_css 'a.edit-post', text: 'Edit'
+    end
+
+    
+
+  end
+end
