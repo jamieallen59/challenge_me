e903819cb742541865c1dea038eadc1d327bec3a
Joe Dowdell <joedowdell@gmail.com>
2014-09-03 10:51:08 +0100

    Joe & Nikesh | Feature test for users logged out

--- spec/features/welcome_feature_spec.rb
+++ spec/features/welcome_feature_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 describe 'users experience' do
 
-	it 'when visiting challenge-me index page users see homepage' do
+	it 'when visiting challenge.me index page users see homepage' do
 		visit '/'
 		expect(page).to have_content 'Make giving fun.
 Complete extra challenged to reach your fundraising targets'
+	end
+
+	context 'when users is logged out' do
+
+			it 'when visiting challenge.me users can log in' do
+				visit '/'
+				expect(page).to have_link('Login', href: new_user_session_path)
+			end
+
 	end
 
 end
