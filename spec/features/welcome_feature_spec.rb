5ac662c4aa5bfe28d3144b4ad9e879167d1282e6
Nikesh Ashar <nikeshashar@gmail.com>
2014-09-05 14:54:24 +0100

    Chloe fixing search box text

--- spec/features/welcome_feature_spec.rb
+++ spec/features/welcome_feature_spec.rb
@@ -7,6 +7,11 @@ describe 'users experience' do
 		expect(page).to have_content "WE MAKE GIVING FUN COMPLETE CHALLENGES. SMASH TARGETS. LET'S GO"
 	end
 
+	it 'when landing on homepage expect to see search box' do
+		visit '/'
+		expect(page).to have_css 'input.find_person'
+	end
+
 	context 'when users is logged out' do
 
 			it 'when visiting challenge.me users can log in' do
