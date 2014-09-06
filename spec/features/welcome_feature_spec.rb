983058658425619d9c5755104e0303404088dc13
csharpd <chloedonegan@gmail.com>
2014-09-06 11:00:35 +0100

    Testing new landing page format - let's go setup etc

--- spec/features/welcome_feature_spec.rb
+++ spec/features/welcome_feature_spec.rb
@@ -12,14 +12,18 @@ describe 'users experience' do
 		expect(page).to have_css 'input.find_person'
 	end
 
-
-	context 'when users is logged out' do
-
-			it 'when visiting challenge.me users can log in' do
-				visit '/'
-				expect(page).to have_link("LET'S GO", href: select_events_path)
-			end
-
+	it "when landing on homepage expect to see LET'S GO button" do
+		expect(page).to have_link("LET'S GO", href: select_events_path)
 	end
 
+	context 'when users is logged in' do
+		before do
+			@mary = create(:user)
+			login_as @mary
+		end
+			it 'when visiting challenge.me users can log out' do
+				visit '/'
+				expect(page).to have_link(rsp'Sign Out', href: destroy_user_session)
+			end
+	end
 end
