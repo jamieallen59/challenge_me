c3376550b6473dd6682909518f9ae3334987295f
csharpd <chloedonegan@gmail.com>
2014-09-07 17:23:44 +0100

    Nicola & Chloe | Events page - My events button

--- spec/features/display_event_page_feature_spec.rb
+++ spec/features/display_event_page_feature_spec.rb
@@ -1,31 +1,36 @@
 require 'rails_helper'
 
-describe 'event page' do
+describe 'show event page' do
+  before do
+   @mary = create(:user)
+   @event = create(:event, user: @mary)
+  end
   context 'with posts' do
-    before do
-      mary = create(:user)
-      @event = create(:event, user: mary)
-      Post.create(caption: 'Test Post ABC', event_id: @event.id, user: mary)
-    end
     it 'should display posts' do
+      Post.create(caption: 'Test Post ABC', event_id: @event.id, user: @mary)
       visit event_path(@event)
       expect(page).to have_content 'Test Post ABC'
     end
   end
-end
 
-describe 'show event page' do
-  before do
-    mary = create(:user)
-    @event = create(:event, user: mary)
- end
-
- it 'should query the event donations action for fundraising data' do
+  it 'should query the event donations action for fundraising data' do
     fundraising = double :fundraising, page: { 'grandTotalRaisedExcludingGiftAid' => 100 }
     allow(JustGiving::Fundraising).to receive(:new).and_return(fundraising)
 
     visit event_path(@event)
     expect(page).to have_content 'raised £10.0 so far'
     expect(page).to have_content '1% of fundraising target achieved'
+  end
+
+ context 'creators can access their other events from profile page' do
+  before do
+    login_as @mary
+  end
+  it 'by clicking my events button' do
+    visit event_path(@event)
+    click_link 'My Events'
+    expect(current_path).to eq select_events_path
+  end
+
  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -17,14 +17,16 @@ RSpec.describe Post, :type => :model do
       expect(post).to have(1).error_on(:caption)
     end
 
-    it 'is not valid if the video url is not from youtube' do 
+    it 'is not valid if the video url is not from youtube' do
       post = Post.create(caption: "I'm so speedy", video: "www.facebook.com")
       expect(post).to have(1).error_on(:video)
     end
 
+
+
   end
 
-  context '#is_owner?' do 
+  context '#is_owner?' do
     before do
       @mary = create(:user)
       @fred = create(:fred)
@@ -32,7 +34,7 @@ RSpec.describe Post, :type => :model do
       @post = create(:post, event: @event, user: @mary)
     end
 
-    it 'should return true if user is owner of the post' do 
+    it 'should return true if user is owner of the post' do
       expect(@post.is_owner?(@mary)).to be true
     end
 
@@ -40,7 +42,7 @@ RSpec.describe Post, :type => :model do
       expect(@post.is_owner?(@fred)).to be false
     end
 
-    it 'should return false if nil user is passed in' do 
+    it 'should return false if nil user is passed in' do
       expect(@post.is_owner?(nil)).to be false
     end
   end
