aa677b4387832cce415ce65d2c11f9a0c14f17b9
csharpd <chloedonegan@gmail.com>
2014-09-07 16:13:03 +0100

    Nicola & Chloe | enabled text posts

--- spec/features/creating_posts_feature_spec.rb
+++ spec/features/creating_posts_feature_spec.rb
@@ -36,11 +36,20 @@ describe 'creating posts' do
 			expect(current_path).to eq event_path(@event)
 		end
 
-		it 'can attach a video link to a post' do 
+		it 'can attach a video link to a post' do
 			fill_in 'Caption', with: 'This is me doing the cinnamon challenge'
 			fill_in 'Video', with: "https://www.youtube.com/watch?v=GJ3xR_oqeEQ"
 			click_button 'Create Post'
 			expect(page).to have_css '.video-post'
+		end
+
+		it 'can create a text post' do
+			fill_in 'Caption', with: 'September Update'
+			fill_in 'Text', with: 'Hi guys, training is going really well'
+			click_button 'Create Post'
+			expect(page).to have_css '.text-post'
+			expect(page).to have_content 'September Update'
+			expect(page).to have_content 'Hi guys, training is going really well'
 		end
 	end
 
