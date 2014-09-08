645967bb8d1cc3a89dfd6051bde8ac679a22b6ef
Toan Nguyen <toan@graduate.uwa.edu.au>
2014-09-08 11:42:41 +0100

    Toan & Nikesh | Added timeline for posts

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,6 +3,12 @@
 FactoryGirl.define do
   factory :post do
     caption "Run Roger"
-    picture File.new(Rails.root.join('app/assets/images/run.jpg'))
+    text "hello"
+    factory(:video) do
+      video "https://www.youtube.com/watch?v=eT4LgPwP19o"
+    end
+    factory(:picture) do
+      picture File.new(Rails.root.join('app/assets/images/run.jpg'))
+    end
   end
 end
