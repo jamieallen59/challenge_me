1276f6948545edfaf05b6831d99fa0bf296f2433
Nikesh Ashar <nikeshashar@gmail.com>
2014-09-10 00:20:02 +0100

    Added config status to application rb

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.initialize_on_precompile = false
+
 module ChallengeMe
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
