237faa6480cb4e367873afdb693bcf034b993c1f
Nikesh Ashar <nikeshashar@gmail.com>
2014-09-09 12:58:27 +0100

    Added Javascript mini target bar to events_path

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,137 +1,140 @@
+function showMiniProgressBar(miniAmountRaised, miniTargetAmount, eventID) {
+    var minitargetbar = new TargetBar(miniAmountRaised, miniTargetAmount, eventID);
+    var $event = $('[data-event-id=' + eventID + ']')
+
+    var getPercent = (minitargetbar.percentage()) / 100;
+    var getTargetWrapWidth = $('.mini-target-wrap').width();
+    var progressTotal = getPercent * getTargetWrapWidth;
+    var animationLength = 2500;
+
+    // Display target amount
+    $event.find('.mini-target-amount').text(minitargetbar.miniTargetAmount);
+    // Display money raised so far
+    $event.find('.money-raised').text(minitargetbar.amount);
+    // Add current progress as a class to indicate colour of the bar
+    $event.find('.mini-target-wrap').addClass(minitargetbar.progress())
+    
+
+    // on page load, animate percentage bar to current donation percentage
+    // .stop() used to prevent animation queueing
+    console.log($event.html());
+    $event.find('.mini-target-bar').stop().animate({
+        left: progressTotal
+    }, animationLength);
+}
 
 $(document).ready(function(){
 
-  //gets the donation amount
-  var url = document.URL + '/donations.json';
-  $.get(url, function(data) {
-    var amountRaised = "£" + data.donationTotal + " raised so far"
-    var percentageRaised = data.donationPercentage + "% of fundraising target achieved."
-    $('#amount-raised').text(amountRaised);
-    $('.percentage-completion').text(percentageRaised);
-  });
+  if($('#event-page').length) {
+    //gets the donation amount
+    var url = document.URL + '/donations.json';
+    $.get(url, function(data) {
+      var amountRaised = "£" + data.donationTotal + " raised so far"
+      var percentageRaised = data.donationPercentage + "% of fundraising target achieved."
+      $('#amount-raised').text(amountRaised);
+      $('.percentage-completion').text(percentageRaised);
+    });
 
-  var addAttribute = function(attributeId, type, label) {
-    $(attributeId).attr(type, label);
+    var addAttribute = function(attributeId, type, label) {
+      $(attributeId).attr(type, label);
+    }
+
+    var removeAttribute = function(attributeId, label){
+      $(attributeId).removeAttr(label);
+    }
+
+    var removeClass = function(attributeClass, label) {
+      $(attributeClass).removeClass(label);
+    }
+
+    var addClass = function(attributeClass, label) {
+      $(attributeClass).addClass(label);
+    }
+
+    $('#2-workouts-label').on('click', function(){
+      removeClass('.label-checked', 'label-checked');
+      addClass('#2-workouts-label', 'label-checked');
+      removeAttribute('input[type="radio"]', 'checked')
+      addAttribute('#2-workouts', 'checked', 'checked');
+    });
+
+    $('#3-workouts-label').on('click', function(){
+      removeClass('.label-checked', 'label-checked');
+      addClass('#3-workouts-label', 'label-checked');
+      removeAttribute('input[type="radio"]', 'checked');
+      addAttribute('#3-workouts', 'checked', 'checked');
+    });
+
+    $('#4-workouts-label').on('click', function(){
+      removeClass('.label-checked', 'label-checked');
+      addClass('#4-workouts-label', 'label-checked');
+      removeAttribute('input[type="radio"]', 'checked');
+      addAttribute('#4-workouts', 'checked', 'checked');
+    });
+
+    $('#5-workouts-label').on('click', function(){
+      removeClass('.label-checked', 'label-checked');
+      addClass('#5-workouts-label', 'label-checked');
+      removeAttribute('input[type="radio"]', 'checked');
+      addAttribute('#5-workouts', 'checked', 'checked');
+    });
+
+    $('#6-workouts-label').on('click', function(){
+      removeClass('.label-checked', 'label-checked');
+      addClass('#6-workouts-label', 'label-checked');
+      removeAttribute('input[type="radio"]', 'checked');
+      addAttribute('#6-workouts', 'checked', 'checked');
+    });
+
+    function displayModal(e){
+    e.preventDefault();
+    $($(e.target).attr('data-modal-selector')).modal({
+        fadeDuration: 500,
+        fadeDelay: 0.30
+    });
   }
 
-  var removeAttribute = function(attributeId, label){
-    $(attributeId).removeAttr(label);
-  }
-
-  var removeClass = function(attributeClass, label) {
-    $(attributeClass).removeClass(label);
-  }
-
-  var addClass = function(attributeClass, label) {
-    $(attributeClass).addClass(label);
-  }
-
-  $('#2-workouts-label').on('click', function(){
-    removeClass('.label-checked', 'label-checked');
-    addClass('#2-workouts-label', 'label-checked');
-    removeAttribute('input[type="radio"]', 'checked')
-    addAttribute('#2-workouts', 'checked', 'checked');
-  });
-
-  $('#3-workouts-label').on('click', function(){
-    removeClass('.label-checked', 'label-checked');
-    addClass('#3-workouts-label', 'label-checked');
-    removeAttribute('input[type="radio"]', 'checked');
-    addAttribute('#3-workouts', 'checked', 'checked');
-  });
-
-  $('#4-workouts-label').on('click', function(){
-    removeClass('.label-checked', 'label-checked');
-    addClass('#4-workouts-label', 'label-checked');
-    removeAttribute('input[type="radio"]', 'checked');
-    addAttribute('#4-workouts', 'checked', 'checked');
-  });
-
-  $('#5-workouts-label').on('click', function(){
-    removeClass('.label-checked', 'label-checked');
-    addClass('#5-workouts-label', 'label-checked');
-    removeAttribute('input[type="radio"]', 'checked');
-    addAttribute('#5-workouts', 'checked', 'checked');
-  });
-
-  $('#6-workouts-label').on('click', function(){
-    removeClass('.label-checked', 'label-checked');
-    addClass('#6-workouts-label', 'label-checked');
-    removeAttribute('input[type="radio"]', 'checked');
-    addAttribute('#6-workouts', 'checked', 'checked');
-  });
-
-  function displayModal(e){
-  e.preventDefault();
-  $($(e.target).attr('data-modal-selector')).modal({
-      fadeDuration: 500,
-      fadeDelay: 0.30
-  });
-}
-
-  $(".modal-button").on('click', displayModal);
-  $(".pledge-modal-button").on('click', displayModal);
-  $(".new-post-modal-button").on('click', displayModal);
-  $(".menu-modal-button").on('click', displayModal);
-  $(".challenge-me-button").on('click', displayModal);
+    $(".modal-button").on('click', displayModal);
+    $(".pledge-modal-button").on('click', displayModal);
+    $(".new-post-modal-button").on('click', displayModal);
+    $(".menu-modal-button").on('click', displayModal);
+    $(".challenge-me-button").on('click', displayModal);
 
 
-moveProgressBar();
-  $(window).resize(function() {
-      moveProgressBar();
-  });
+  moveProgressBar();
+    $(window).resize(function() {
+        moveProgressBar();
+    });
 
-  function moveProgressBar() {
-      var targetbar = new TargetBar(amountRaised, targetAmount);
+    function moveProgressBar() {
+        var targetbar = new TargetBar(amountRaised, targetAmount);
 
-      var getPercent = (targetbar.percentage()) / 100;
-      var getTargetWrapWidth = $('.target-wrap').width();
-      var progressTotal = getPercent * getTargetWrapWidth;
-      var animationLength = 2500;
+        var getPercent = (targetbar.percentage()) / 100;
+        var getTargetWrapWidth = $('.target-wrap').width();
+        var progressTotal = getPercent * getTargetWrapWidth;
+        var animationLength = 2500;
 
-      // Display target amount
-      $('.target-amount').text(targetbar.targetAmount);
-      // Display money raised so far
-      $('.money-raised').text(targetbar.amount);
-      // Add current progress as a class to indicate colour of the bar
-      $('.target-wrap').addClass(targetbar.progress())
-      
+        // Display target amount
+        $('.target-amount').text(targetbar.targetAmount);
+        // Display money raised so far
+        $('.money-raised').text(targetbar.amount);
+        // Add current progress as a class to indicate colour of the bar
+        $('.target-wrap').addClass(targetbar.progress())
+        
 
-      // on page load, animate percentage bar to current donation percentage
-      // .stop() used to prevent animation queueing
-      $('.target-bar').stop().animate({
-          left: progressTotal
-      }, animationLength);
-  }
+        // on page load, animate percentage bar to current donation percentage
+        // .stop() used to prevent animation queueing
+        $('.target-bar').stop().animate({
+            left: progressTotal
+        }, animationLength);
+    }
 
 
 
-  minimoveProgressBar();
-  $(window).resize(function() {
-      minimoveProgressBar();
-  });
-
-  function minimoveProgressBar() {
-      var targetbar = new TargetBar(amountRaised, targetAmount);
-
-      var getPercent = (targetbar.percentage()) / 100;
-      var getTargetWrapWidth = $('.mini-target-wrap').width();
-      var progressTotal = getPercent * getTargetWrapWidth;
-      var animationLength = 2500;
-
-      // Display target amount
-      $('.target-amount').text(targetbar.targetAmount);
-      // Display money raised so far
-      $('.money-raised').text(targetbar.amount);
-      // Add current progress as a class to indicate colour of the bar
-      $('.target-wrap').addClass(targetbar.progress())
-      
-
-      // on page load, animate percentage bar to current donation percentage
-      // .stop() used to prevent animation queueing
-      $('.mini-target-bar').stop().animate({
-          left: progressTotal
-      }, animationLength);
+    minimoveProgressBar();
+    $(window).resize(function() {
+        minimoveProgressBar();
+    });
   }
 });
 
