60748b63865a2ae4a958a5779a7ed319530991a5
Toan Nguyen <toan@graduate.uwa.edu.au>
2014-09-09 14:14:12 +0100

    Nicola & Toan | creating posts after workouts, including the MMF map

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,8 +1,3 @@
 class Challenge < ActiveRecord::Base
-	# belongs_to :event
-  after_create :create_challenge_post
 
-  def create_challenge_post
-    # event.posts.create(info: self.name)
-  end
 end
