cbf79705a512f765c9fb71ef5836e81b6bdf1aaf
Nikesh Ashar <nikeshashar@gmail.com>
2014-09-10 00:21:29 +0100

    Updated app rb file

--- config/application.rb
+++ config/application.rb
@@ -13,10 +13,11 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
 
 module ChallengeMe
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
